e3c4aa1da31e31db85d97a3512a2ba28836e11fb
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-09-03 02:46:46 -0700

    feat: init app

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image("https://github.com/ANG13T/url_genie/blob/main/webapp/assets/url_genie_logo.png?raw=true")
+
+with col3:
+    st.write(' ')
