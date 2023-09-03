88d67f255f345e2ca601bde5ead27bbc69e48046
Angelina Tsuboi <angelina.t1832@gmail.com>
2023-09-03 02:52:09 -0700

    feat: update logo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ with col1:
     st.write(' ')
 
 with col2:
-    st.image("https://github.com/ANG13T/url_genie/blob/main/webapp/assets/url_genie_logo.png?raw=true")
+    st.image("https://github.com/ANG13T/fingerprint-biosec/blob/main/assets/biometrics_logo.png?raw=true")
 
 with col3:
     st.write(' ')
